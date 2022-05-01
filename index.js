323c4f47d45c8d9811d3f3e2c0e27b5a4c47590e
see <upcwangying@126.com>
2022-05-01 21:37:00 +0800

    ci: microsoft azure

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.get('/', (req, res) => {
   res.json({
     'hello': 'world'
   })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('ready!')
 })
